048d0fcce9db7b1b1ea223b5491f6bbfda3e10f3
Muntyq <aleix.muntal78@gmail.com>
2026-06-05 21:19:23 +0200

    progress

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,4 +1,18 @@
 {
-	inputs = { ... };
+	description = "Rust development shell";
 
-	outpus = { self
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		rust-overlay = {
+			url = "github:oxalica/rust-overlay";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outpus = { self, nixpkgs, rust-overlay, ... };
+		let
+			systems = [ "x86_64-linux" "aarch64-linux" ];
+			overlays = [ rust-overlay.overlays.default ];
+		};
+		in {
+			devShells =
